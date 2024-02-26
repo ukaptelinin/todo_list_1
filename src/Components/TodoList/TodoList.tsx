9de54cd5a109634dfdac431b29e2174b9b10ccb3
Kaptelinin <100-21@mail.ru>
2024-02-26 20:42:45 +0300

    Feature of functional buttons added

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -1,14 +1,29 @@
 import { useContext, FC } from 'react';
-import { TodosStateContext } from '../TodosStateContextProvider/context';
+import { TodosStateContext, TodoItem } from '../TodosStateContextProvider/context';
 import style from './TodoList.module.css';
 import ListItem from './ListItem/ListItem';
 
 const TodoList: FC = () => {
-    const { itemsList } = useContext(TodosStateContext);
+    const { itemsList, todoTypeRender } = useContext(TodosStateContext);
+    let todoListCopy: TodoItem[] = [];
+
+    switch (todoTypeRender) {
+    case 'ALL':
+        todoListCopy = itemsList.slice();
+        break;
+    case 'ACTIVE':
+        todoListCopy = itemsList.filter((item) => !item.isDone);
+        break;
+    case 'COMPLETED':
+        todoListCopy = itemsList.filter((item) => item.isDone);
+        break;
+    default:
+        console.log('error');
+    }
     if (itemsList.length) {
         return (
             <ul className={style.list}>
-                {itemsList.map((item) => (
+                {todoListCopy.map((item) => (
                     <ListItem
                         key={item.id}
                         {...item}
