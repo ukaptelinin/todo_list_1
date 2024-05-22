ea57d4ea613af5fb6d035bebad7838a824f9045a
Kaptelinin <100-21@mail.ru>
2024-05-22 16:27:45 +0300

    Last changes of this branch

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import {
 } from 'react-router-dom';
 import { Container, Paper } from '@mui/material';
 
+import TodosStateContext, { todoListStore } from './Components/TodosStateContext/context';
+import TodoPagination from './Components/TodoPagination/TodoPagination';
 import TodoHeader from './Components/TodoHeader/TodoHeader';
 import TodoInput from './Components/TodoInput/TodoInput';
 import TodoList from './Components/TodoList/TodoList';
 import TodoFooter from './Components/TodoFooter/TodoFooter';
-import TodoPagination from './Components/TodoPagination/TodoPagination';
-import TodosStateContext, { todoListStore } from './Components/TodosStateContext/context';
 
 const App: FC = () => (
 
