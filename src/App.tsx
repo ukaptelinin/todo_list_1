6424aa7b2388f50bdb2c21966d71a32a2067f23c
Kaptelinin <100-21@mail.ru>
2024-09-16 19:00:18 +0300

    Last changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import AppHeader from './Components/AppHeader/AppHeader';
 import TodosStateContext, { todosStore } from './Components/TodosStateContext/TodosStateContext';
 import RootRoutes from './Components/RootRoutes/RootRoutes';
-import { DndProvider } from 'react-dnd/dist/core/DndProvider';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const App: FC = () => (
     <TodosStateContext.Provider value={todosStore}>
-        <BrowserRouter>
-           
-                <AppHeader />
-                <RootRoutes />
-           
+        <BrowserRouter>      
+            <AppHeader />
+            <RootRoutes />   
         </BrowserRouter>
     </TodosStateContext.Provider>
 );
